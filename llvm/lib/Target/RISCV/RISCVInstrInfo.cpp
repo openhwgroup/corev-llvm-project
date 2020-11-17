e229494fb53dec2644e219a3c7b5d4c5ac901e76
flip1995 <philipp.krones@embecosm.com>
2020-11-17 15:32:17 +0000

    [RISCV] Define operands for hardware loop instructions
    Signed-off-by: Philipp Krones <philipp.krones@embecosm.com>

--- llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
+++ llvm/lib/Target/RISCV/RISCVInstrInfo.cpp
@@ -1268,6 +1268,7 @@ bool RISCVInstrInfo::verifyInstruction(const MachineInstr &MI,
   case RISCVOp::OPERAND_UIMM##NUM:                                             \
     Ok = isUInt<NUM>(Imm);                                                     \
     break;
+	CASE_OPERAND_UIMM(1)
         CASE_OPERAND_UIMM(2)
         CASE_OPERAND_UIMM(3)
         CASE_OPERAND_UIMM(4)
