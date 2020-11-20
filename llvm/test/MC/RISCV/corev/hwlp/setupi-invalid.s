521e7921776bdf32b856528d3e9487539dea94ca
Philipp Krones <philipp.krones@embecosm.com>
2020-11-20 14:47:46 +0100

    [COREV] Fix operand order in cv.setupi instruction (#2)
    Signed-off-by: Philipp Krones <philipp.krones@embecosm.com>

--- llvm/test/MC/RISCV/corev/hwlp/setupi-invalid.s
+++ llvm/test/MC/RISCV/corev/hwlp/setupi-invalid.s
@@ -1,22 +1,22 @@
 # RUN: not llvm-mc -triple=riscv32 --mattr=+xcorevhwlp %s 2>&1 \
 # RUN:        | FileCheck %s --check-prefixes=CHECK-ERROR
 
-cv.setupi 1, 31, 0
+cv.setupi 1, 0, 31
 # CHECK-ERROR: immediate must be an even integer in the range [0, 30]
 
-cv.setupi 1, 32, 0
+cv.setupi 1, 0, 32
 # CHECK-ERROR: immediate must be an even integer in the range [0, 30]
 
-cv.setupi 0, -6, 0
+cv.setupi 0, 0, -6
 # CHECK-ERROR: immediate must be an even integer in the range [0, 30]
 
-cv.setupi 0, -7, 0
+cv.setupi 0, 0, -7
 # CHECK-ERROR: immediate must be an even integer in the range [0, 30]
 
-cv.setupi 1, 30, 4096
+cv.setupi 1, 4096, 30
 # CHECK-ERROR: immediate must be an integer in the range [0, 4095]
 
-cv.setupi 1, 30, -10
+cv.setupi 1, -10, 30
 # CHECK-ERROR: immediate must be an integer in the range [0, 4095]
 
 cv.setupi 2, 0, 0
@@ -28,10 +28,10 @@ cv.setupi -1, 0, 0
 cv.setupi s2, 0, 0
 # CHECK-ERROR: immediate must be an integer in the range [0, 1]
 
-cv.setupi 0, s2, 0
+cv.setupi 0, 0, s2
 # CHECK-ERROR: immediate must be an even integer in the range [0, 30]
 
-cv.setupi 0, 0, s2
+cv.setupi 0, s2, 0
 # CHECK-ERROR: immediate must be an integer in the range [0, 4095]
 
 cv.setupi 2
