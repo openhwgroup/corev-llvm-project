6c71e4a7e44569891a9b5a0b888a57499a6e8965
Qihan Cai <qcai8733@uni.sydney.edu.au>
2023-03-06 23:57:15 +1100

    [RISCV] Fix MAC instructions
    Fix #38, change the wrongly implemented cv.mulurn and mulhhurn, and change relevant tests to reflect the fix.

--- llvm/test/MC/RISCV/corev/mac-all-extensions.s
+++ llvm/test/MC/RISCV/corev/mac-all-extensions.s
@@ -7,11 +7,11 @@
 
 cv.mac t0, t1, t2
 # CHECK-INSTR: cv.mac t0, t1, t2
-# CHECK-ENCODING: [0xb3,0x02,0x73,0x42]
+# CHECK-ENCODING: [0xab,0x32,0x73,0x90]
 
 cv.msu t0, t1, t2
 # CHECK-INSTR: cv.msu t0, t1, t2
-# CHECK-ENCODING: [0xb3,0x12,0x73,0x42]
+# CHECK-ENCODING: [0xab,0x32,0x73,0x92]
 
 # 16x16 bit MUL instructions
 
@@ -25,11 +25,11 @@ cv.mulhhs t0, t1, t2
 
 cv.mulsn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulsn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x02,0x73,0x80]
+# CHECK-ENCODING: [0xdb,0x42,0x73,0x00]
 
 cv.mulhhsn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulhhsn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x02,0x73,0xc0]
+# CHECK-ENCODING: [0xdb,0x42,0x73,0x40]
 
 cv.mulsrn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulsrn t0, t1, t2, 0
@@ -49,50 +49,50 @@ cv.mulhhu t0, t1, t2
 
 cv.mulun t0, t1, t2, 0
 # CHECK-INSTR: cv.mulun t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x02,0x73,0x00]
+# CHECK-ENCODING: [0xdb,0x52,0x73,0x00]
 
 cv.mulhhun t0, t1, t2, 0
 # CHECK-INSTR: cv.mulhhun t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x02,0x73,0x40]
+# CHECK-ENCODING: [0xdb,0x52,0x73,0x40]
 
 cv.mulurn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulurn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x42,0x73,0x00]
+# CHECK-ENCODING: [0xdb,0x52,0x73,0x80]
 
 cv.mulhhurn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulhhurn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x42,0x73,0x40]
+# CHECK-ENCODING: [0xdb,0x52,0x73,0xc0]
 
 # 16x16 bit MAC instructions
 
 cv.macsn t0, t1, t2, 0
 # CHECK-INSTR: cv.macsn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x12,0x73,0x80]
+# CHECK-ENCODING: [0xdb,0x62,0x73,0x00]
 
 cv.machhsn t0, t1, t2, 0
 # CHECK-INSTR: cv.machhsn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x12,0x73,0xc0]
+# CHECK-ENCODING: [0xdb,0x62,0x73,0x40]
 
 cv.macsrn t0, t1, t2, 0
 # CHECK-INSTR: cv.macsrn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x52,0x73,0x80]
+# CHECK-ENCODING: [0xdb,0x62,0x73,0x80]
 
 cv.machhsrn t0, t1, t2, 0
 # CHECK-INSTR: cv.machhsrn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x52,0x73,0xc0]
+# CHECK-ENCODING: [0xdb,0x62,0x73,0xc0]
 
 cv.macun t0, t1, t2, 0
 # CHECK-INSTR: cv.macun t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x12,0x73,0x00]
+# CHECK-ENCODING: [0xdb,0x72,0x73,0x00]
 
 cv.machhun t0, t1, t2, 0
 # CHECK-INSTR: cv.machhun t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x12,0x73,0x40]
+# CHECK-ENCODING: [0xdb,0x72,0x73,0x40]
 
 cv.macurn t0, t1, t2, 0
 # CHECK-INSTR: cv.macurn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x52,0x73,0x00]
+# CHECK-ENCODING: [0xdb,0x72,0x73,0x80]
 
 cv.machhurn t0, t1, t2, 0
 # CHECK-INSTR: cv.machhurn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x52,0x73,0x40]
+# CHECK-ENCODING: [0xdb,0x72,0x73,0xc0]
