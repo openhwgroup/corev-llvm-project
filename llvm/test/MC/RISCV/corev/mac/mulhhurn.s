024f07c01b017adc10572abe44c7b4b2258a485c
realqhc <98016705+realqhc@users.noreply.github.com>
2023-03-10 19:36:59 +1100

    [RISCV] Fix MAC instructions (#39)
    Fix #38, change the wrongly implemented cv.mulurn and mulhhurn, and change relevant tests to reflect the fix.

--- llvm/test/MC/RISCV/corev/mac/mulhhurn.s
+++ llvm/test/MC/RISCV/corev/mac/mulhhurn.s
@@ -3,12 +3,12 @@
 
 cv.mulhhurn t0, t1, t2, 0
 # CHECK-INSTR: cv.mulhhurn t0, t1, t2, 0
-# CHECK-ENCODING: [0xdb,0x52,0x73,0x40]
+# CHECK-ENCODING: [0xdb,0x52,0x73,0xc0]
 
 cv.mulhhurn t0, t1, zero, 16
 # CHECK-INSTR: cv.mulhhurn t0, t1, zero, 16
-# CHECK-ENCODING: [0xdb,0x52,0x03,0x60]
+# CHECK-ENCODING: [0xdb,0x52,0x03,0xe0]
 
 cv.mulhhurn t0, t1, zero, 31
 # CHECK-INSTR: cv.mulhhurn t0, t1, zero, 31
-# CHECK-ENCODING: [0xdb,0x52,0x03,0x7e]
+# CHECK-ENCODING: [0xdb,0x52,0x03,0xfe]
