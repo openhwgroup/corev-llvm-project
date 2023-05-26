c8c9023284759150fa1ca70442d8c2c6d33ba040
Qihan Cai <qcai8733@uni.sydney.edu.au>
2023-05-26 12:46:35 +1000

    [RISCV][CodeGen] Implement XcvMac Intrinsics
    Implement XcvMac intrinsics in CodeGen.
    
    Spec: https://github.com/openhwgroup/core-v-sw/blob/master/specifications/corev-builtin-spec.md

--- llvm/lib/Support/RISCVISAInfo.cpp
+++ llvm/lib/Support/RISCVISAInfo.cpp
@@ -107,6 +107,7 @@ static const RISCVSupportedExtension SupportedExtensions[] = {
     {"xcvsimd", RISCVExtensionVersion{1, 0}},
     {"xcvbitmanip", RISCVExtensionVersion{1, 0}},
     {"xcvbi", RISCVExtensionVersion{1, 0}},
+    {"xcvmac", RISCVExtensionVersion{1, 0}}
 };
 
 static const RISCVSupportedExtension SupportedExperimentalExtensions[] = {
