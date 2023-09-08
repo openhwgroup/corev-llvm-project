8cb5834b445acaedd36dffe99d100f89a969ee86
Qihan Cai <qcai8733@uni.sydney.edu.au>
2023-09-08 17:46:50 +1000

    [RISCV] Update naming of cv.slet

--- llvm/test/MC/RISCV/corev/alu/slet.s
+++ llvm/test/MC/RISCV/corev/alu/slet.s
@@ -1,10 +1,18 @@
 # RUN: llvm-mc -triple=riscv32 --mattr=+xcvalu -show-encoding %s \
 # RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INSTR
 
+cv.sle t0, t1, t2
+# CHECK-INSTR: cv.sle t0, t1, t2
+# CHECK-ENCODING: [0xab,0x32,0x73,0x52]
+
+cv.sle a0, a1, a2
+# CHECK-INSTR: cv.sle a0, a1, a2
+# CHECK-ENCODING: [0x2b,0xb5,0xc5,0x52]
+
 cv.slet t0, t1, t2
-# CHECK-INSTR: cv.slet t0, t1, t2
+# CHECK-INSTR: cv.sle t0, t1, t2
 # CHECK-ENCODING: [0xab,0x32,0x73,0x52]
 
 cv.slet a0, a1, a2
-# CHECK-INSTR: cv.slet a0, a1, a2
+# CHECK-INSTR: cv.sle a0, a1, a2
 # CHECK-ENCODING: [0x2b,0xb5,0xc5,0x52]
--- llvm/test/MC/RISCV/corev/alu/sletu-invalid.s
+++ llvm/test/MC/RISCV/corev/alu/sletu-invalid.s
@@ -1,6 +1,21 @@
 # RUN: not llvm-mc -triple=riscv32 --mattr=+xcvalu %s 2>&1 \
 # RUN:        | FileCheck %s --check-prefixes=CHECK-ERROR
 
+cv.sleu t0, t1, 0
+# CHECK-ERROR: invalid operand for instruction
+
+cv.sleu t0, 0, t2
+# CHECK-ERROR: invalid operand for instruction
+
+cv.sleu 0, t1, t2
+# CHECK-ERROR: invalid operand for instruction
+
+cv.sleu t0, t1
+# CHECK-ERROR: too few operands for instruction
+
+cv.sleu t0, t1, t2, a0
+# CHECK-ERROR: invalid operand for instruction
+
 cv.sletu t0, t1, 0
 # CHECK-ERROR: invalid operand for instruction
 
